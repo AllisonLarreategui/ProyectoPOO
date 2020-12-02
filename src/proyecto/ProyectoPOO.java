bd778f30365a9bfa6cbff80d2d80f9fa1adaace7
Allison Larreategui <alarreat@espol.edu.ec>
2020-12-02 16:48:38 -0500

    sout de prueba

--- src/proyecto/ProyectoPOO.java
+++ src/proyecto/ProyectoPOO.java
@@ -17,6 +17,7 @@ public class ProyectoPOO {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hi mundo");
+        System.out.println("Semestre  AP");
     }
     
 }
