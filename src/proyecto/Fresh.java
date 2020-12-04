9d16ca38f0c7cf34faf5a5adfddb64d24a594426
Allison Larreategui <alarreat@espol.edu.ec>
2020-12-04 10:30:27 -0500

    Despliegue del menu

--- src/proyecto/Fresh.java
+++ src/proyecto/Fresh.java
@@ -6,10 +6,13 @@
 package proyecto;
 
 public class Fresh extends Cliente{
+    
+    public Fresh(String cedula, String nombre, String apellido, String telefono, String correo, String direccion, float precioMensual) {
+       super(cedula, nombre, apellido, telefono, correo, direccion, precioMensual);
+    }
 
     @Override
-    public double calcularSuscripcion(int dias) {
+    public double calcularSuscripcion(int dias){
         return 0;
     }
-    
 }
