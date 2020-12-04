e91f06262efb82bc29fe2dadf1ac4e2eb6164fe5
Dschica <dschica@espol.edu.ec>
2020-12-04 17:13:26 -0500

    c

--- src/proyecto/Ingrediente.java
+++ src/proyecto/Ingrediente.java
@@ -10,5 +10,20 @@ package proyecto;
  * @author Diana Chica Z
  */
 public class Ingrediente {
+    String nombre;
+    int calorias;
+    int hidratos;
+    int proteinas;
+    int grasas;
+    int fibras;
+    
+    public Ingrediente(String nombre, int calorias, int hidratos, int proteinas, int grasas, int fibras) {
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.hidratos = hidratos;
+        this.proteinas = proteinas;
+        this.grasas = grasas;
+        this.fibras = fibras;}
+    
     
 }
